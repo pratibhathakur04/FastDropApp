bff5a6d00d3cdc7bf9a9dfacb2c2c4d67d219635
pratibhathakur04 <thakurpratibha370@gmail.com>
2025-02-21 15:35:02 +0530

    hi these are some backend files

---
+++ backend/src/config/db.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+
+ module.exports = connectDB;
+// const mongoose = require("mongoose");
+
+// const connectDatabase = () => {
+//   mongoose
+//     .connect(process.env.DB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//     })
+//     .then((data) => {
+//       console.log(`Mongodb connected with server: ${data.connection.host}`);
+//     });
+// };
+
+// module.exports = connectDatabase;
---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+connectDB();
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
